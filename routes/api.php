6ec7aa4fed4e1941cb2c171bc34103289c34f1b6
Masiré Fofana <masire.fofana@natixis.com>
2020-10-12 17:53:47 +0200

    Add auth middleware to api resources

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources([
-    'alerts' => 'API\AlertController',
-    'clients' => 'API\ClientController',
-    'watched-automated-processes' => 'API\WatchedAutomatedProcessController',
-    'ui-path-orchestrators' => 'API\UiPathOrchestratorController',
-    'alert-triggers' => 'API\AlertTriggerController'
-]);
+Route::group(["middleware"=>"api:auth"], function() {
+    Route::apiResources([
+        'alerts' => 'API\AlertController',
+        'clients' => 'API\ClientController',
+        'watched-automated-processes' => 'API\WatchedAutomatedProcessController',
+        'ui-path-orchestrators' => 'API\UiPathOrchestratorController',
+        'alert-triggers' => 'API\AlertTriggerController'
+    ]);
+});
