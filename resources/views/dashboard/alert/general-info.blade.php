8d877627605027a6b475877ba395fd9c4bf4a3b9
Masiré Fofana <masire.fofana@natixis.com>
2020-10-20 11:29:56 +0200

    Bug fix

--- resources/views/dashboard/alert/general-info.blade.php
+++ resources/views/dashboard/alert/general-info.blade.php
@@ -2,6 +2,11 @@
     <span class="icon has-text-{{ $alert->definition->level }}">
         <i class="fas fa-burn"></i>
     </span>
+    <span class="has-tooltip-right" data-tooltip="Latest heartbeat {{ $alert->latest_heartbeat_at ? $alert->latestHeartbeatAtDiffForHumans() : $alert->createdAtDiffForHumans() }}">
+        <span class="icon heartbeat has-text-{{ $alert->alive ? 'success' : 'grey-light' }}">
+            <i class="fas fa-heartbeat"></i>
+        </span>
+    </span>
     <strong>#{{ str_pad($alert->id, 4, '0', STR_PAD_LEFT) }}</strong> | {{ $alert->trigger->title }}
     <small class="has-tooltip-right" data-tooltip="{{ $alert->createdAt() }}">
         (Raised {{ $alert->createdAtDiffForHumans() }})
